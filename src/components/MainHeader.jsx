491cf9b0dc1b1c14d846836e5669a265d2961f15
nathanmintegui <nathanberger.rs@hotmail.com>
2025-08-30 20:31:41 -0300

    feat: add navbar icons proper routing

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -6,8 +6,10 @@ function MainHeader() {
     <div className="w-full flex justify-between items-center p-4">
       {/* Logo */}
       <div className="flex space-x-3">
-      <img className="h-12" src={logoLoja} alt="logo da loja" />
-      <h1 className="text-2xl font-bold p-2">Moto Spectro</h1>
+        <a href="/">
+          <img className="h-12 hover:opacity-85 transition duration-250 ease-in-out" src={logoLoja} alt="logo da loja" />
+        </a>
+        <h1 className="text-2xl font-bold p-2">Moto Spectro</h1>
       </div>
 
       {/* Barra de busca */}
@@ -18,19 +20,21 @@ function MainHeader() {
           className="w-full px-4 py-2 outline-none"
         />
         <button className="bg-red-600 hover:bg-red-700 text-white px-4 flex items-center gap-2">
-          <Search size={18} />  
+          <Search size={18} />
         </button>
       </div>
 
       {/* Favoritos e Carrinho */}
       <div className="flex items-center gap-6">
-        <Heart className="cursor-pointer hover:text-red-600" size={22} />
-        <div className="relative flex items-center gap-2 cursor-pointer">
+        <a href="/desejos">
+          <Heart className="cursor-pointer hover:text-red-600" size={22} />
+        </a>
+        <a href="/reservas" className="relative flex items-center gap-2 cursor-pointer">
           <ShoppingBag size={22} className="hover:text-red-600" />
           <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
-            
+
           </span>
-        </div>
+        </a>
       </div>
     </div>
   );
